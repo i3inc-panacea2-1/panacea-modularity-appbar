c3e66c1b5793c51e6e63576431c444e128cd2e9d
Portno <j.remoundos@dotbydot.gr>
2019-07-01 16:46:39 +0300

    first commit

---
+++ src/Panacea.Modularity.AppBar/IAppBar.cs
@@ -0,0 +1,20 @@
+﻿using Panacea.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.AppBar
+{
+    public interface IAppBar
+    {
+        void Show();
+
+        void Hide();
+
+        void AddButton(ViewModelBase vm);
+
+        void RemoveButton(ViewModelBase vm);
+    }
+}
---
+++ src/Panacea.Modularity.AppBar/PanaceaServicesExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Panacea.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.AppBar
+{
+    public static class PanaceaServicesExtensions
+    {
+        public static bool TryGetAppBar(this PanaceaServices core, out IAppBar appbar)
+        {
+            appbar = core.PluginLoader.GetPlugins<IAppBarPlugin>().FirstOrDefault()?.GetAppBar();
+            return appbar != null;
+        }
+    }
+}
